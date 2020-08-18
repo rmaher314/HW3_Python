c604a4ae4a87ed3193497ce5a9ed3017bc26630e
Regan <regan_maher@outlook.com>
2020-08-18 15:14:57 -0500

    For Average Change, was able to convert the number to only have 2 decimals.  Got "a" fuction working for the greatest increase and decrease but the calculation is still not working right.

--- Py_Bank/Py_Bank.py
+++ Py_Bank/Py_Bank.py
@@ -24,19 +24,21 @@ with open(bank_data) as csvfile:
     for row in csvreader:
         month_counter = (month_counter + 1)
         profit_total = (profit_total + int(row[1]))
-        if month_counter != 1:
-            diff = (int(row[1]) - int(prev_val))
+        if int(month_counter) != 1:
+            diff = (int(row[1]) - int(prev_val))        
         total_diff = (total_diff + diff)
         prev_val= int(row[1])
-    average_change = total_diff/month_counter
+    average_change = total_diff/(month_counter - 1)
+    average_change = str(round(average_change, 2))
+    #This one is not quite right amount is 
+    greatest_increase = (max(row[1]))
+    greatest_decrease = (min(row[1]))
+    month= (row[0])
     print(f"Total Months: " + str(month_counter))
-    print(f"Total Profit: {profit_total}" )
-    print(f"Average Change:  {average_change}")    
-
-#Define the two variables in the document.
-#def month = row[0]
-#def profit_loses = row[1] 
-    #print(month)
+    print(f"Total Profit: ${profit_total}" )
+    print(f"Average Change:  ${average_change}")    
+    print(f"Greatest Increase in Profits:  {month} ${greatest_increase}")
+    print(f"Greatest Decrease in Profits:  {month} ${greatest_decrease}")
 
 #Total count of months (column 1)- will need to do a count of all of the months should equal 86.
        #def Total_Months len(month)
@@ -62,9 +64,5 @@ with open(bank_data) as csvfile:
 
 #Find the min profit/Change
         #def min(min_profit):
-            #min_so_far = min_profit[0]
-            #for number in numbers:
-                #if number < min_so_far:
-                #min_so_far = number
-            #return min_so_far
+            
             #print("Greatest Decrease in Profits:  ", max_profit)
