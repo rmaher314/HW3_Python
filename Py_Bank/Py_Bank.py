c8f87f3b3e0f6321f6d008e00db981e0450df3ac
Regan <regan_maher@outlook.com>
2020-08-18 18:14:16 -0500

    Got Py Bank to fully execute all required outputs.  Need to figure out how to export to text file.

--- Py_Bank/Py_Bank.py
+++ Py_Bank/Py_Bank.py
@@ -11,60 +11,44 @@ with open(bank_data) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     
     csv_header = next(csvreader)
-    print(f"Header: {csv_header}"  )
+    #print(f"Header: {csv_header}"  )
 
-#Counting the number of months and total
+#declaration lists
     month_counter = 0
     profit_total = 0
     diff = 0
     prev_val = 0
     total_diff = 0
+    greatest_increase = 0
+    greatest_decrease = 0
+    great_month = ""
+    least_month = ""
     
-#for reader 
+#for loop for csv reader
+#for loop for all output components
     for row in csvreader:
         month_counter = (month_counter + 1)
         profit_total = (profit_total + int(row[1]))
         if int(month_counter) != 1:
-            diff = (int(row[1]) - int(prev_val))        
+            diff = (int(row[1]) - int(prev_val))
+            if greatest_increase < diff:
+                greatest_increase = diff 
+                great_month = row[0]
+            if greatest_decrease > diff:
+                greatest_decrease = diff 
+                least_month = row[0]      
         total_diff = (total_diff + diff)
         prev_val= int(row[1])
+    
     average_change = total_diff/(month_counter - 1)
     average_change = str(round(average_change, 2))
-    #This one is not quite right amount is 
-    greatest_increase = max(row[1])
-    greatest_decrease = (min(row[1]))
-    month= (row[0])
+
+#Final print statements
     print("Financial Analysis")
     print("-----------------------------")
     print(f"Total Months: " + str(month_counter))
     print(f"Total Profit: ${profit_total}" )
     print(f"Average Change:  ${average_change}")    
-    print(f"Greatest Increase in Profits:  {month} ${greatest_increase}")
-    print(f"Greatest Decrease in Profits:  {month} ${greatest_decrease}")
+    print(f"Greatest Increase in Profits:  {great_month} ${greatest_increase}")
+    print(f"Greatest Decrease in Profits:  {least_month} ${greatest_decrease}")
 
-#Total count of months (column 1)- will need to do a count of all of the months should equal 86.
-       #def Total_Months len(month)
-        #print("Total Months: ", Total_Months))
-
-#Total sum of Profit/Losses.  Total the entire column.
-        #def Total_Profit sum(profit_loses)
-        # print("Total Profit/Loses:  ", Total_Profit))
-
-
-# Find the average change a month.
-        #def Average_Change(Total_Profit/Total_Months) 
-        #print("Average Change:  ", Average_Change)
-
-# Find the max profit/Change
-        #def max(max_profit):
-            #max_so_far = max_profit[0]
-            #for profit_loses in profit:
-                #if profit_loses < max_so_far:
-                    #max_so_far = max_profit
-            #return max_profit
-            #print("Greatest Increase in Profits:  ", max_profit)
-
-#Find the min profit/Change
-        #def min(min_profit):
-            
-            #print("Greatest Decrease in Profits:  ", max_profit)
