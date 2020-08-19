ae381e31cc83cd1668ff8dfef279ab89e1ad14af
Regan <regan_maher@outlook.com>
2020-08-19 15:45:27 -0500

    For Py_Poll - got the votes to count correctly.

--- Py_Poll/Py_Poll.py
+++ Py_Poll/Py_Poll.py
@@ -2,12 +2,32 @@ import os
 import csv
 
 #Accessing the csv file.
-election_data = os.path.join("..","Py_Poll", "election_data.csv")
+election_data = os.path.join("..","Py_Poll","Resources","election_data.csv")
 print(election_data, type(election_data))
+
+#Exporting to a text file.
+election_results = os.path.join("Election_Results","Election_Results.txt")
 
 #with open(output_path, 'r') as csvfile:
 with open(election_data) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     
     csv_header = next(csvreader)
-    print(f"Header: {csv_header}"  )
+    #print(f"Header: {csv_header}"  )
+
+#declaration lists
+    khan_counter = 0
+    correy_counter = 0
+    li_counter = 0
+    otooley_counter = 0  
+    total_votes = 0
+
+#for loop for all output components
+    for row in csvreader:
+        total_votes = (total_votes + 1)  
+
+
+#Final Print Statements
+    print(f"Election Results")
+    print(f"-----------------------------")
+    print(f"Total Votes:   {str(total_votes)}")
